0eee65dccb1eda7f52269256e92de8d70921bd9d
hunainparekh2211006KHICND <hunain.parekh@xloopdigital.com>
2022-12-16 12:04:24 +0500

    get name and id from api

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -9,12 +9,28 @@ const Pokemon = ({name})=>{
         }
     });
     let image;
+    let pokemon_name;
+    let pokemon_code;
     if(placeholder.length !== 0){
         image = <img className='image' src={placeholder.sprites.other.home.front_default} alt="poke_image"/>;
-        // console.log(placeholder.sprites.other.home.front_default);
+        pokemon_name = <h3>{placeholder.name}</h3>
+        if(placeholder.id <10){
+            pokemon_code = <h3>#00{placeholder.id}</h3>
+        }
+        else{
+            pokemon_code = <h3>#0{placeholder.id}</h3>
+        }
     }
     return <div className='pokemon-content'>
-        {image}
+        <div className='heading'>
+            {pokemon_name}
+            {pokemon_code}
+        </div>
+        <div className='image-div'>
+            {image}
+        </div>
+        
+        
     </div>
       
 }
